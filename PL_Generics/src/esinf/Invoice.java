3185788515dc406679190bd53166c9c3d7406f42
Nuno Teixeira <1210869@isep.ipp.pt>
2022-09-27 13:06:38 +0100

    Adjusts code;

--- PL_Generics/src/esinf/Invoice.java
+++ PL_Generics/src/esinf/Invoice.java
@@ -8,54 +8,64 @@ package esinf;
 import java.time.LocalDate;
 
 /**
- *
  * @author DEI-ISEP
  */
-public class Invoice implements Comparable<Invoice> {
+public class Invoice implements Comparable<Invoice>
+{
     private String reference;
     private LocalDate date;
-    
-    public Invoice(String reference, String date) {
-        this.reference=reference;
-        if (date!=null) {
+
+    public Invoice(String reference, String date)
+    {
+        this.reference = reference;
+        if (date != null)
+        {
             String s[] = date.split("/");
             this.date = LocalDate.of(Integer.parseInt(s[0]), Integer.parseInt(s[1]), Integer.parseInt(s[2]));
         }
     }
-    
-    public Invoice(String reference) {
-        this(reference, "1900/01/01");
-    }    
 
-    public String getReference() {
+    public Invoice(String reference)
+    {
+        this(reference, "1900/01/01");
+    }
+
+    public String getReference()
+    {
         return reference;
     }
 
-    public void setReference(String reference) {
+    public void setReference(String reference)
+    {
         this.reference = reference;
     }
 
-    public LocalDate getDate() {
+    public LocalDate getDate()
+    {
         return date;
     }
 
-    public void setDate(LocalDate date) {
+    public void setDate(LocalDate date)
+    {
         this.date = date;
     }
 
-   @Override
-    public boolean equals(Object obj) {
-       
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
     @Override
-    public int hashCode() {
+    public boolean equals(Object obj)
+    {
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public int compareTo(Invoice o) {
+    public int hashCode()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int compareTo(Invoice o)
+    {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
--- PL_Generics/src/esinf/Product.java
+++ PL_Generics/src/esinf/Product.java
@@ -6,61 +6,72 @@
 package esinf;
 
 /**
- *
  * @author DEI-ISEP
  */
-public class Product implements Comparable<Product> {
+public class Product implements Comparable<Product>
+{
     private String identification;
     private int quantity;
     private long price;
-    
-    Product(String identification, int quantity, long price ) {
+
+    Product(String identification, int quantity, long price)
+    {
         this.identification = identification;
         this.quantity = quantity;
         this.price = price;
     }
-    
-    Product(String identification ) {
-        this(identification,0,0);
+
+    Product(String identification)
+    {
+        this(identification, 0, 0);
     }
 
-    public String getIdentification() {
+    public String getIdentification()
+    {
         return identification;
     }
 
-    public void setIdentification(String identification) {
+    public void setIdentification(String identification)
+    {
         this.identification = identification;
     }
 
-    public int getQuantity() {
+    public int getQuantity()
+    {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(int quantity)
+    {
         this.quantity = quantity;
     }
 
-    public long getPrice() {
+    public long getPrice()
+    {
         return price;
     }
 
-    public void setPrice(long price) {
+    public void setPrice(long price)
+    {
         this.price = price;
     }
-    
+
     @Override
-    public boolean equals(Object obj) {
-       
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    
-    @Override
-    public int hashCode() {
+    public boolean equals(Object obj)
+    {
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public int compareTo(Product p) {
+    public int hashCode()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int compareTo(Product p)
+    {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
